35fd12a75de6f7082a9f273be37964dfd1b43759
Naoto Tanzawa <154235099+NaotoTanzawa@users.noreply.github.com>
2024-01-24 22:01:45 +0900

    JavaScriptの基礎を学ぼう　課題 四則演算をしよう　を再提出

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,10 +1,12 @@
 // 変数の宣言
 let userWeight;
 let userHeight;
+let bmi
 
 // 値の代入
 userWeight=68;
 userHeight=1.7;
+bmi=userWeight/(userHeight**2);
 
 // コンソールへの出力
-console.log(userWeight/(userHeight**2))
+console.log(bmi);
